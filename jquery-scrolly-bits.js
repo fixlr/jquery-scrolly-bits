419956a285f64d22b140986387c484ab4019451f
Nathan Fixler <nathan@fixler.org>
2012-01-16 14:25:50 -0500

    More refactoring

--- jquery-scrolly-bits.js
+++ jquery-scrolly-bits.js
@@ -1,33 +1,41 @@
 (function( $ ) {
-  $.fn.exists = function() {
-    return (this.length > 0);
-  }
+  $.fn.scrollyBits = function() {
+    var target, target_top, hash;
 
-  $.fn.doAwesomeScrollingThing = function() {
-    return this.each(function() {
-      $(this).click(function(event) {
-        hash = this.href.split('#')[1];
-        target = $('#' + hash);
+    return $(this).click(function(event) {
+      hash = getLinkTarget(this);
+      target = $('#' + hash);
 
-        if (target.exists()) {
-          var target_top = target.offset().top;
-          $('html, body').animate({ scrollTop: target_top }, 500, function() {
-            document.location.hash = hash;
-          })
-          event.preventDefault();
-        }
-      });
+      if (target.exists()) {
+        target_top = target.offset().top;
+
+        $('html, body').animate({ scrollTop: target_top }, 500, function() {
+          document.location.hash = hash;
+        })
+        event.preventDefault();
+      }
     });
   }
 
-  $.fn.isOmniPresentAllSeeingNav = function() {
-    flatNav = this.first().clone().appendTo('body').addClass('flat').hide();
+  $.fn.copyNav = function(options) {
+    var settings = $.extend({
+      'navClass': 'flat'
+    }, options);
+
+    return this.first()
+      .clone()
+      .appendTo('body')
+      .addClass(settings['navClass'])
+      .hide();
   }
 
   $.fn.setCurrentSection = function() {
+    var target;
+
     if (this.is(':visible')) {
       this.find('a').each(function() {
-        target = $('#' + this.href.split('#')[1]);
+        target = $('#' + getLinkTarget(this));
+
         if ((target.offset().top + target.height()) > ($(window).scrollTop() + 50)) {
           $('.nav.flat a').removeClass('current');
           $(this).addClass('current');
@@ -38,14 +46,24 @@
   }
 
   $(window).scroll(function() {
-    menu = $('.nav.flat');
+    var menu = $('.nav.flat'),
+        windowPosition = $(window).scrollTop(),
+        menuVisible = menu.is(':visible');
 
-    if (($(window).scrollTop() > 500) && (!menu.is(':visible'))) {
+    if (windowPosition > 500 && !menuVisible) {
       menu.fadeIn();
     }
-    else if (($(window).scrollTop() < 500) && (menu.is(':visible'))) {
+    else if (windowPosition < 500 && menuVisible) {
       menu.fadeOut();
     }
     menu.setCurrentSection();
   });
+
+  $.fn.exists = function() {
+    return (this.length > 0);
+  }
+
+  var getLinkTarget = function(link) {
+    return link.href.split('#')[1];
+  }
 })( jQuery );
