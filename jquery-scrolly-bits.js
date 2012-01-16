f166beb1f07e4cd2d7b929faa1a8ec08f042eb1c
Nathan Fixler <nathan@fixler.org>
2012-01-16 13:40:36 -0500

    Semicolons

--- jquery-scrolly-bits.js
+++ jquery-scrolly-bits.js
@@ -1,51 +1,51 @@
 (function( $ ) {
   $.fn.exists = function() {
-    return (this.length > 0)
+    return (this.length > 0);
   }
 
   $.fn.doAwesomeScrollingThing = function() {
     return this.each(function() {
       $(this).click(function(event) {
-        hash = this.href.split('#')[1]
-        target = $('#' + hash)
+        hash = this.href.split('#')[1];
+        target = $('#' + hash);
 
         if (target.exists()) {
-          var target_top = target.offset().top
+          var target_top = target.offset().top;
           $('html, body').animate({ scrollTop: target_top }, 500, function() {
-            document.location.hash = hash
+            document.location.hash = hash;
           })
-          event.preventDefault()
+          event.preventDefault();
         }
-      })
-    })
+      });
+    });
   }
 
   $.fn.isOmniPresentAllSeeingNav = function() {
-    flatNav = this.first().clone().appendTo('body').addClass('flat').hide()
+    flatNav = this.first().clone().appendTo('body').addClass('flat').hide();
   }
 
   $.fn.setCurrentSection = function() {
     if (this.is(':visible')) {
       this.find('a').each(function() {
-        target = $('#' + this.href.split('#')[1])
+        target = $('#' + this.href.split('#')[1]);
         if ((target.offset().top + target.height()) > ($(window).scrollTop() + 50)) {
-          $('.nav.flat a').removeClass('current')
-          $(this).addClass('current')
-          return false
+          $('.nav.flat a').removeClass('current');
+          $(this).addClass('current');
+          return false;
         }
-      })
+      });
     }
   }
 
   $(window).scroll(function() {
-    menu = $('.nav.flat')
+    menu = $('.nav.flat');
 
     if (($(window).scrollTop() > 500) && (!menu.is(':visible'))) {
-      menu.fadeIn()
+      menu.fadeIn();
     }
     else if (($(window).scrollTop() < 500) && (menu.is(':visible'))) {
-      menu.fadeOut()
+      menu.fadeOut();
     }
-    menu.setCurrentSection()
-  })
+    menu.setCurrentSection();
+  });
 })( jQuery );
