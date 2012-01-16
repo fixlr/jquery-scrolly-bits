75853ebe774b662d915cb5f2c0114b85970b91eb
Nathan Fixler <nathan@fixler.org>
2012-01-16 13:31:00 -0500

    Moved plugin code into a closure

--- jquery-scrolly-bits.js
+++ jquery-scrolly-bits.js
@@ -1,49 +1,51 @@
-jQuery.fn.exists = function() {
-  return (this.length > 0)
-}
+(function( $ ) {
+  $.fn.exists = function() {
+    return (this.length > 0)
+  }
 
-jQuery.fn.doAwesomeScrollingThing = function() {
-  return this.each(function() {
-    $(this).click(function(event) {
-      hash = this.href.split('#')[1]
-      target = $('#' + hash)
+  $.fn.doAwesomeScrollingThing = function() {
+    return this.each(function() {
+      $(this).click(function(event) {
+        hash = this.href.split('#')[1]
+        target = $('#' + hash)
 
-      if (target.exists()) {
-        var target_top = target.offset().top
-        $('html, body').animate({ scrollTop: target_top }, 500, function() {
-          document.location.hash = hash
-        })
-        event.preventDefault()
-      }
+        if (target.exists()) {
+          var target_top = target.offset().top
+          $('html, body').animate({ scrollTop: target_top }, 500, function() {
+            document.location.hash = hash
+          })
+          event.preventDefault()
+        }
+      })
     })
+  }
+
+  $.fn.isOmniPresentAllSeeingNav = function() {
+    flatNav = this.first().clone().appendTo('body').addClass('flat').hide()
+  }
+
+  $.fn.setCurrentSection = function() {
+    if (this.is(':visible')) {
+      this.find('a').each(function() {
+        target = $('#' + this.href.split('#')[1])
+        if ((target.offset().top + target.height()) > ($(window).scrollTop() + 50)) {
+          $('.nav.flat a').removeClass('current')
+          $(this).addClass('current')
+          return false
+        }
+      })
+    }
+  }
+
+  $(window).scroll(function() {
+    menu = $('.nav.flat')
+
+    if (($(window).scrollTop() > 500) && (!menu.is(':visible'))) {
+      menu.fadeIn()
+    }
+    else if (($(window).scrollTop() < 500) && (menu.is(':visible'))) {
+      menu.fadeOut()
+    }
+    menu.setCurrentSection()
   })
-}
-
-jQuery.fn.isOmniPresentAllSeeingNav = function() {
-  flatNav = this.first().clone().appendTo('body').addClass('flat').hide()
-}
-
-jQuery.fn.setCurrentSection = function() {
-  if (this.is(':visible')) {
-    this.find('a').each(function() {
-      target = $('#' + this.href.split('#')[1])
-      if ((target.offset().top + target.height()) > ($(window).scrollTop() + 50)) {
-        $('.nav.flat a').removeClass('current')
-        $(this).addClass('current')
-        return false
-      }
-    })
-  }
-}
-
-$(window).scroll(function() {
-  menu = $('.nav.flat')
-
-  if (($(window).scrollTop() > 500) && (!menu.is(':visible'))) {
-    menu.fadeIn()
-  }
-  else if (($(window).scrollTop() < 500) && (menu.is(':visible'))) {
-    menu.fadeOut()
-  }
-  menu.setCurrentSection()
-})
+})( jQuery );
